7785d6a992b138578e1950ee92f249c61cbb0cd4
muise001 <emiel.muis@hva.nl>
2019-06-26 21:28:30 +0200

    Created better slider

--- Site/js.js
+++ Site/js.js
@@ -1,8 +1,6 @@
 const previous = document.getElementById("previous")
 const next = document.getElementById("next")
-const sliderLength = document.querySelectorAll(".mainimage img").length - 1
-const sliderWapper = document.querySelector('.imagewrapper')
-
+const sliderLength = document.querySelectorAll(".sliderNavigator img").length - 1
 let currentImage = 0
 
 if (document.querySelector(".pageHeaderSlider img")) {
@@ -25,28 +23,25 @@ document.querySelectorAll(".mainimage img").forEach((img, i) => {
   `
 })
 
-document.querySelectorAll(".sliderImage").forEach(img => {
+document.querySelectorAll(".sliderNavigator img").forEach((img, i) => {
+  img.classList.add(`img${i}`)
+  document.querySelector(".slider_main").innerHTML += `
+  <div class=img${i}>
+    <img src="${img.src}" alt="">
+  </div>
+  `
+})
+
+document.querySelector(".slider_main").setAttribute("style", `width: ${document.querySelectorAll(".sliderNavigator img").length}00%`)
+
+document.querySelectorAll(".sliderNavigator img").forEach(img => {
   img.addEventListener("click", e => {
-    currentImage = parseInt(img.classList[1].replace("img", ""))
+    currentImage = parseInt(img.classList[0].replace("img", ""))
     validate()
   })
 })
 
-const validate = () => {
-  sliderWapper.style.transform = `translateX(-${currentImage}00%)`
-  if (currentImage < 1) {
-    previous.classList.add("none")
-    next.classList.remove("none")
-  } else if(currentImage === sliderLength) {
-    next.classList.add("none")
-    previous.classList.remove("none")
-  } else {
-    next.classList.remove("none")
-    previous.classList.remove("none")
-  }
-}
-
-next.addEventListener("click", () => {
+document.getElementById("next").addEventListener("click", () => {
   if (currentImage < sliderLength) {
     currentImage++
     validate()
@@ -59,6 +54,24 @@ previous.addEventListener("click", () => {
     validate()
   }
 })
+
+const validate = () => {
+  if (document.querySelector(".slider_main > div.img0")) {
+    document.querySelectorAll(".slider_main > div").forEach(img => {
+      img.style.transform = `translateX(-${currentImage}00%)`
+    })
+  }
+  if (currentImage < 1) {
+    previous.classList.add("none")
+    next.classList.remove("none")
+  } else if(currentImage === sliderLength) {
+    next.classList.add("none")
+    previous.classList.remove("none")
+  } else {
+    next.classList.remove("none")
+    previous.classList.remove("none")
+  }
+}
 
 // translateX(-100%) Op de tweede Foto
 // translateX(-200%) Op de derde Foto
